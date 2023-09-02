810c17116997d81151c055a934e8e557fdeafeb1
edson@app100.com.br <mueledson>
2023-09-02 19:23:34 -0300

    A função de Completar Tarefa tá pronta

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,14 @@
-const Todo = ({ todo, removeTodo }) => {
+import React from "react";
+
+const Todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div
-      className="todo"
-    >
+    <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" : "none"}}>
       <div className="content">
-        <p>{todo.text}</p>
+        <h3>{todo.text}</h3>
         <p className="category">({todo.category})</p>
       </div>
       <div>
-        <button className="complete">
+        <button className="complete" onClick={() => completeTodo(todo.id)}>
           Completar
         </button>
         <button className="remove" onClick={() => removeTodo(todo.id)}>
